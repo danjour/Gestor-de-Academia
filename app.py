9cc966faa3043bf416850f3cda0bc2b4b57bcfc9
Eduardo D'anjour <eduardo.djs@outlook.com>
2025-01-19 21:22:50 -0300

    modified:   app.py 	new file:   static/register.css     new file:   templates/register.html

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import sqlite3
 
 app = Flask(__name__)
 
@@ -11,6 +12,20 @@ def index():
 @app.route('/login')
 def login():
     return render_template('clients/login.html')
+
+#Generic route for register a new client
+@app.route('/new_client')
+def new_client():
+    return render_template('manager/register.html')
+
+#For a while, it will be simple, but in the next step, it will become more complex as we put the database in a secure place.
+@app.route('/cadastrar', methods=['GET', 'POST'])
+def cadastrar():
+    if request.method == 'POST':
+        pass
+        return redirect('/login')  
+
+    return render_template('cadastro.html')
 
 
 if __name__ == '__main__':
