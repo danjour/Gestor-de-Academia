395ae93a4412670b92b599060690fc3fc8f88f05
Eduardo D'anjour <eduardo.djs@outlook.com>
2025-01-19 21:10:39 -0300

    Add login route and create login page template

--- app.py
+++ app.py
@@ -7,6 +7,12 @@ def index():
     return render_template('index.html')
 
 
+#Generic route for login page
+@app.route('/login')
+def login():
+    return render_template('clients/login.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
     app.run(host='0.0.0.0', port=5000, debug=True)
